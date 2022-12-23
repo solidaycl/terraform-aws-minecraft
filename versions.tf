bcd58413c13207447dfcaf82028323aa7fa347ec
Chris Soliday <solidaycl@gmail.com>
2022-12-23 16:17:54 -0500

    Updated terraform version

--- versions.tf
+++ versions.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = "~> 0.12.24"
+  required_version = "~> 1.3.6"
 
   required_providers {
-    aws      = "~> 2.58.0"
+    aws      = "~> 2.9.9"
     template = "~> 2.1.2"
   }
 }
